e371051c274526543d54cdc0f4b380710b0dd829
xaviguasch <xaviguasch@gmail.com>
2019-06-04 12:20:55 +0200

    refactored app after the context split

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -2,12 +2,12 @@ import React, { useContext } from 'react'
 import TextField from '@material-ui/core/TextField'
 import Paper from '@material-ui/core/Paper'
 import useInputState from './hooks/useInputState'
-import { TodosContext } from './context/todos.context'
+import { DispatchContext } from './context/todos.context'
 
 function TodoForm() {
   const [value, handleChange, reset] = useInputState('')
 
-  const { dispatch } = useContext(TodosContext)
+  const dispatch = useContext(DispatchContext)
 
   return (
     <Paper style={{ margin: '1rem 0', padding: '0 1rem' }}>
--- src/context/todos.context.js
+++ src/context/todos.context.js
@@ -1,8 +1,6 @@
 import React, { createContext, useReducer } from 'react'
 import todoReducer from '../reducers/todo.reducer'
 
-import useTodoState from '../hooks/useTodoState'
-
 const defaultTodos = [
   { id: 1, task: 'Mow the lawn', completed: false },
   { id: 2, task: 'Release lady bugs into garden', completed: true }
@@ -15,8 +13,8 @@ export function TodosProvider(props) {
   const [todos, dispatch] = useReducer(todoReducer, defaultTodos)
 
   return (
-    <TodosContext.Provider value={{ todos }}>
-      <DispatchContext.Provider value={{ dispatch }}>
+    <TodosContext.Provider value={todos}>
+      <DispatchContext.Provider value={dispatch}>
         {props.children}
       </DispatchContext.Provider>
     </TodosContext.Provider>
